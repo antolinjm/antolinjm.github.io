f4e5aeab14a8b0b9359ef58e5cb9858a7d9b2385
antolinjm <antolinjm@users.noreply.github.com>
2026-02-26 14:31:34 +0100

    Update Google Analytics tracking ID

--- js/analytics.js
+++ js/analytics.js
@@ -1,4 +1,4 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-XXXXXXXXXX'); // replace with your real ID
+gtag('config', 'G-QPMH0CL7N2'); // replace with your real ID
